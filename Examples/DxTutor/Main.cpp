13696815b144ef6ac52b70009f47a3f6d694b1e0
Kirill Markin <kmarkindev@gmail.com>
2025-02-26 15:46:45 +0300

    Fix missing rainbow dorito

--- Examples/DxTutor/Main.cpp
+++ Examples/DxTutor/Main.cpp
@@ -1,3 +1,5 @@
+#define WINDOWS_LEAN_AND_MEAN
+
 import <array>;
 import <stdexcept>;
 import <vector>;
@@ -6,6 +8,24 @@ import <iostream>;
 import "directx/d3dx12.h";
 import <dxgi1_6.h>;
 import <windows.h>;
+import <d3dcompiler.h>;
+
+import <DirectXMath.h>;
+
+struct Check
+{
+    const char* outMsg = "HRESULT failed";
+};
+
+bool operator >> (HRESULT Result, const Check& ck)
+{
+    if(FAILED(Result))
+    {
+        throw std::runtime_error(ck.outMsg);
+    }
+
+    return true;
+}
 
 HWND Window {};
 
@@ -75,18 +95,15 @@ void InitRender()
 
     #if defined(_DEBUG)
     {
-        Microsoft::WRL::ComPtr<ID3D12Debug> DebugController;
-        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&DebugController))))
-        {
-            DebugController->EnableDebugLayer();
+        Microsoft::WRL::ComPtr<ID3D12Debug1> DebugController;
 
-            // Enable additional debug layers.
-            DxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
-        }
-        else
-        {
-            throw std::runtime_error("Failed to create debug layer");
-        }
+        D3D12GetDebugInterface(IID_PPV_ARGS(&DebugController)) >> Check{"Failed to create debug layer"};
+
+        DebugController->EnableDebugLayer();
+        //DebugController->SetEnableGPUBasedValidation(true);
+
+        // Enable additional debug layers.
+        DxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
     }
     #endif
 
@@ -101,11 +118,10 @@ void InitRender()
     // Создаем Device объект, позволяющий работать с выбранным адаптером.
 
     Microsoft::WRL::ComPtr<IDXGIAdapter> Adapter {};
-    for(UINT AdapterIndex = 0; SUCCEEDED(Factory->EnumAdapterByGpuPreference(
+    for(UINT AdapterIndex = 0; Factory->EnumAdapterByGpuPreference(
             AdapterIndex,
             DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
-            IID_PPV_ARGS(&Adapter))
-        ); ++AdapterIndex)
+            IID_PPV_ARGS(&Adapter)) >> Check{"Failed to enumerate adapters"}; ++AdapterIndex)
     {
         if(SUCCEEDED(D3D12CreateDevice(
             Adapter.Get(),
@@ -124,7 +140,7 @@ void InitRender()
 
     {
         DXGI_ADAPTER_DESC Desc {};
-        Adapter->GetDesc(&Desc);
+        Adapter->GetDesc(&Desc) >> Check{"Failed to get adapter description"};
         std::wcout << "Selected adapter: " << Desc.Description << std::endl;
     }
 
@@ -135,10 +151,7 @@ void InitRender()
     queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
     queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 
-    if(FAILED(Device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&CommandQueue))))
-    {
-        throw std::runtime_error("Failed to create command queue");
-    }
+    Device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&CommandQueue)) >> Check {"Failed to create command queue"};
 
     // 4. Создаем Swap Chain
 
@@ -153,19 +166,16 @@ void InitRender()
 
 
     Microsoft::WRL::ComPtr<IDXGISwapChain1> SwapChain1 {};
-    if(FAILED(Factory->CreateSwapChainForHwnd(
+    Factory->CreateSwapChainForHwnd(
         CommandQueue.Get(),
         Window,
         &swapChainDesc,
         nullptr,
         nullptr,
         &SwapChain1
-        )))
-    {
-        throw std::runtime_error("Failed to create swap chain");
-    }
+        ) >> Check {"Failed to create swap chain"};
 
-    SwapChain1.As(&SwapChain);
+    SwapChain1.As(&SwapChain) >> Check{};
     SwapChainFrameIndex = SwapChain->GetCurrentBackBufferIndex();
 
     // 5. Создаем descriptor heap
@@ -174,7 +184,7 @@ void InitRender()
     rtvHeapDesc.NumDescriptors = FrameCount;
     rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
     rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-    Device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&DescriptorHeap));
+    Device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&DescriptorHeap)) >> Check {"Failed to create rtv descriptor heap"};
 
     // Размер шага внутри heap, шаг на два дескриптора это 2 * DescriptorHeapIncrementSize
     DescriptorHeapIncrementSize = Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
@@ -187,10 +197,7 @@ void InitRender()
     for (UINT n = 0; n < FrameCount; n++)
     {
         // Получаем Render Target из Swap Chain
-        if(FAILED(SwapChain->GetBuffer(n, IID_PPV_ARGS(&RenderTargets[n]))))
-        {
-            throw std::runtime_error("Failed to get buffer from swap chain");
-        }
+        SwapChain->GetBuffer(n, IID_PPV_ARGS(&RenderTargets[n])) >> Check {"Failed to get buffer from swap chain"};
 
         // Создаем Render Target View в Descriptor Heap
         Device->CreateRenderTargetView(RenderTargets[n].Get(), nullptr, CpuHandle);
@@ -200,32 +207,20 @@ void InitRender()
     }
 
     // 7. Создаем аллокатор для команд
-    if(FAILED(Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&CommandAllocator))))
-    {
-        throw std::runtime_error("Failed to create command allocator");
-    }
+    Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&CommandAllocator)) >> Check {"Failed to create command allocator"};
 
     // 8. Создаем Command List
-    if(FAILED(Device->CreateCommandList(0,
+    Device->CreateCommandList(0,
         D3D12_COMMAND_LIST_TYPE_DIRECT,
         CommandAllocator.Get(),
         nullptr,
-        IID_PPV_ARGS(&CommandList))))
-    {
-        throw std::runtime_error("Failed to create command list");
-    }
+        IID_PPV_ARGS(&CommandList)) >> Check{"Failed to create command list"};
 
-    if(FAILED(CommandList->Close()))
-    {
-        throw std::runtime_error("Failed to close command list");
-    }
+    CommandList->Close() >> Check{"Failed to close command list"};
 
     // 9. Создаем Fence
 
-    if(FAILED(Device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&Fence))))
-    {
-        throw std::runtime_error("Failed to create fence");
-    }
+    Device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&Fence)) >> Check {"Failed to create fence"};
     FenceValue = 1;
 
     // Create an event handle to use for frame synchronization.
@@ -238,10 +233,9 @@ void InitRender()
 
 void WaitFence()
 {
-    CommandQueue->Signal(Fence.Get(), FenceValue);
-    Fence->SetEventOnCompletion(FenceValue, FenceEvent);
+    CommandQueue->Signal(Fence.Get(), ++FenceValue) >> Check{"Failed to signal fence"};
+    Fence->SetEventOnCompletion(FenceValue, FenceEvent) >> Check{"Failed to set fence event"};
     ::WaitForSingleObject(FenceEvent, INFINITE);
-    FenceValue++;
 }
 
 void DestroyRender()
@@ -255,13 +249,170 @@ void Update()
 
 }
 
+Microsoft::WRL::ComPtr<ID3D12Resource> VertexBuffer {};
+D3D12_VERTEX_BUFFER_VIEW VertexBufferView {};
+Microsoft::WRL::ComPtr<ID3D12RootSignature> RootSignature {};
+Microsoft::WRL::ComPtr<ID3D12PipelineState> PipelineState {};
+Microsoft::WRL::ComPtr<ID3DBlob> VertexShader {};
+Microsoft::WRL::ComPtr<ID3DBlob> PixelShader {};
+D3D12_RECT ScissorRect = {0, 0, LONG_MAX, LONG_MAX};
+D3D12_VIEWPORT Viewport {0, 0, 800, 600};
+
+void LoadAssets()
+{
+    // 1. Определяем данные для загрузки
+
+    struct Vertex
+    {
+        DirectX::XMFLOAT3 Position {};
+        DirectX::XMFLOAT4 Color {};
+    };
+
+    Vertex vertexData[] = {
+        { {0.f, 0.5f, 0.f}, {0.3f, 0.5f, 0.7f, 1.f} },
+        { {0.5f, -0.5f, 0.f}, {0.3f, 0.7f, 0.2f, 1.f} },
+        { {-0.5f, -0.5f, 0.f}, {0.5f, 0.3f, 0.7f, 1.f} },
+    };
+
+    // 2. Создаем ресурс для хранения данных на GPU
+    {
+        auto HeapProps =  CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+        auto ResourceDesc = CD3DX12_RESOURCE_DESC::Buffer(sizeof(vertexData));
+
+        Device->CreateCommittedResource(
+            &HeapProps,
+            D3D12_HEAP_FLAG_NONE,
+            &ResourceDesc,
+            D3D12_RESOURCE_STATE_COMMON,
+            nullptr,
+            IID_PPV_ARGS(&VertexBuffer)
+        ) >> Check{"Failed to create vertex buffer"};
+    }
+
+    // 3. Создаем ресурс для загрузки данных в созданный ранее Vertex Buffer
+    // Т.к. мы указали D3D12_HEAP_TYPE_DEFAULT, у нас нет возможности писать в буфер из CPU
+    // Так что мы создаем еще один, и записываем данные через него.
+    // PS. Можно было бы использовать один буфер с другим типом, но тогда страдала бы
+    // производительность. Note: upload buffer удаляется, т.к. он не нужен после загрузки.
+
+    Microsoft::WRL::ComPtr<ID3D12Resource> VertexUploadBuffer {};
+
+    {
+        auto HeapProps =  CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+        auto ResourceDesc = CD3DX12_RESOURCE_DESC::Buffer(sizeof(vertexData));
+
+        Device->CreateCommittedResource(
+                &HeapProps,
+                D3D12_HEAP_FLAG_NONE,
+                &ResourceDesc,
+                D3D12_RESOURCE_STATE_GENERIC_READ,
+                nullptr,
+                IID_PPV_ARGS(&VertexUploadBuffer)
+            ) >> Check{"Failed to create vertex upload buffer"};
+    }
+
+    // 4. Мапим GPU память на CPU память, пишем наши данные в буфер, не забываем Unmap.
+
+    {
+        void* PtrToLoad {};
+        VertexUploadBuffer->Map(0, nullptr, &PtrToLoad) >> Check{"Failed to map vertex buffer to memory"};
+        std::memcpy(PtrToLoad, vertexData, sizeof(vertexData));
+        VertexUploadBuffer->Unmap(0, nullptr);
+    }
+
+    // 5. Как и обещали, копируем данные с upload buffer в обычный buffer.
+
+    CommandAllocator->Reset() >> Check{"Failed to reset command allocator"};
+    CommandList->Reset(CommandAllocator.Get(), nullptr) >> Check{"Failed to reset command list"};
+    CommandList->CopyResource(VertexBuffer.Get(), VertexUploadBuffer.Get());
+
+    {
+        const auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+            VertexBuffer.Get(),
+            D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
+        CommandList->ResourceBarrier(1, &barrier);
+    }
+
+    CommandList->Close() >> Check{"Failed to close command list"};
+
+    ID3D12CommandList* CommandLists[] = { CommandList.Get() };
+    CommandQueue->ExecuteCommandLists(std::size(CommandLists), CommandLists);
+
+    // 6. Создаем View чтобы использовать буфер в будущем.
+
+    VertexBufferView = {
+        .BufferLocation = VertexBuffer->GetGPUVirtualAddress(),
+        .SizeInBytes = sizeof(vertexData),
+        .StrideInBytes = sizeof(Vertex),
+    };
+
+    // 7. Ожидаем выполнение копирования (завершения выполнения Command List)
+
+    WaitFence();
+
+    // 8. Создаем Root Signature и PSO для отрисовки треугольника (радужного бурито, лол)
+
+    {
+        CD3DX12_ROOT_SIGNATURE_DESC RootSignatureDesc = {};
+        RootSignatureDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+        Microsoft::WRL::ComPtr<ID3DBlob> RootSignatureBlob {};
+        Microsoft::WRL::ComPtr<ID3DBlob> RootSignatureErrorBlob {};
+        D3D12SerializeRootSignature(&RootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1_0,
+            &RootSignatureBlob, &RootSignatureErrorBlob) >> Check{"Failed to serialize root signature"};
+        Device->CreateRootSignature(0, RootSignatureBlob->GetBufferPointer(),
+            RootSignatureBlob->GetBufferSize(), IID_PPV_ARGS(&RootSignature)) >> Check{"Failed to create root signature"};
+    }
+
+    {
+        D3D12_INPUT_ELEMENT_DESC InputLayout[] = {
+            { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+            { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+        };
+
+        #if defined(_DEBUG)
+        // Enable better shader debugging with the graphics debugging tools.
+        UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+        #else
+        UINT compileFlags = 0;
+        #endif
+
+        // Note: вместо компиляции на лету, можно скомпилировать их заранее и получить .cso файлы,
+        // которые можно загрузить используя D3DReadFileBlob
+
+        D3DCompileFromFile(L"S:/Dev/my/krendrr/Examples/DxTutor/shaders.hlsl",
+            nullptr, nullptr, "VSMain", "vs_5_0",
+            compileFlags, 0, &VertexShader, nullptr) >> Check{"Failed to compile vertex shader"};
+        D3DCompileFromFile(L"S:/Dev/my/krendrr/Examples/DxTutor/shaders.hlsl",
+            nullptr, nullptr, "PSMain", "ps_5_0",
+            compileFlags, 0, &PixelShader, nullptr) >> Check{"Failed to compile pixel shader"};
+
+        D3D12_GRAPHICS_PIPELINE_STATE_DESC PsoDesc = {};
+        PsoDesc.InputLayout = { InputLayout, std::size(InputLayout) };
+        PsoDesc.pRootSignature = RootSignature.Get();
+        PsoDesc.VS = CD3DX12_SHADER_BYTECODE(VertexShader.Get());
+        PsoDesc.PS = CD3DX12_SHADER_BYTECODE(PixelShader.Get());
+        PsoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+        PsoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+        PsoDesc.DepthStencilState.DepthEnable = FALSE;
+        PsoDesc.DepthStencilState.StencilEnable = FALSE;
+        PsoDesc.SampleMask = UINT_MAX;
+        PsoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+        PsoDesc.NumRenderTargets = 1;
+        PsoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+        PsoDesc.SampleDesc.Count = 1;
+
+        Device->CreateGraphicsPipelineState(&PsoDesc, IID_PPV_ARGS(&PipelineState)) >> Check{"Failed to create graphics pipeline"};
+    }
+}
+
 void Render()
 {
     int CurrentSwapIndex = SwapChain->GetCurrentBackBufferIndex();
     auto& Rtv = RenderTargets[CurrentSwapIndex];
 
-    CommandAllocator->Reset();
-    CommandList->Reset(CommandAllocator.Get(), nullptr);
+    CommandAllocator->Reset() >> Check{"Failed to reset command allocator"};
+    CommandList->Reset(CommandAllocator.Get(), nullptr) >> Check{"Failed to reset command list"};
 
     auto BarrierToRender = CD3DX12_RESOURCE_BARRIER::Transition(Rtv.Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
     CommandList->ResourceBarrier(1, &BarrierToRender);
@@ -272,15 +423,28 @@ void Render()
     CD3DX12_CPU_DESCRIPTOR_HANDLE RtvHandle { DescriptorHeap->GetCPUDescriptorHandleForHeapStart(), CurrentSwapIndex, DescriptorHeapIncrementSize };
     CommandList->ClearRenderTargetView(RtvHandle, ClearColor, 0, nullptr);
 
+    CommandList->SetPipelineState(PipelineState.Get());
+    CommandList->SetGraphicsRootSignature(RootSignature.Get());
+
+    CommandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    CommandList->IASetVertexBuffers(0, 1, &VertexBufferView);
+
+    CommandList->RSSetViewports(1, &Viewport);
+    CommandList->RSSetScissorRects(1, &ScissorRect);
+
+    CommandList->OMSetRenderTargets(1, &RtvHandle, true, nullptr);
+
+    CommandList->DrawInstanced(3, 1, 0, 0);
+
     auto BarrierToPresent = CD3DX12_RESOURCE_BARRIER::Transition(Rtv.Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
     CommandList->ResourceBarrier(1, &BarrierToPresent);
 
-    CommandList->Close();
+    CommandList->Close() >> Check{"Failed to close command list"};
 
     ID3D12CommandList* CommandLists[] = { CommandList.Get() };
     CommandQueue->ExecuteCommandLists(std::size(CommandLists), CommandLists);
 
-    SwapChain->Present(0, 0);
+    SwapChain->Present(0, 0) >> Check{"Failed to present swap chain"};
 
     WaitFence();
 }
@@ -289,6 +453,7 @@ int main() try
 {
     CreateRenderWindow();
     InitRender();
+    LoadAssets();
 
     MSG Msg = {};
     bool Exit = false;
