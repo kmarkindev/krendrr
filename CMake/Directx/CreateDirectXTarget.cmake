16a30bfeda48f0c02dd5c29969dc9ab071274ed6
Kirill Markin <kmarkindev@gmail.com>
2025-05-12 17:00:19 +0300

    Prepare target for phong demo

--- CMake/Directx/CreateDirectXTarget.cmake
+++ CMake/Directx/CreateDirectXTarget.cmake
@@ -11,23 +11,23 @@ target_include_directories(DirectX12AgilitySdk INTERFACE ${DIRECTX12AGILITYSDK_R
 target_link_libraries(DirectX12AgilitySdk INTERFACE D3D12.lib dxguid.lib Dxgi.lib D3DCompiler.lib)
 
 # Copy DLLs into bin directory
-add_custom_command(
-    OUTPUT
-        ${CMAKE_BINARY_DIR}/bin/D3D12Core.dll
-        ${CMAKE_BINARY_DIR}/bin/d3d12SDKLayers.dll
-    COMMAND
-        ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/bin/
-    COMMAND
-        ${CMAKE_COMMAND} -E copy_if_different
-            ${DIRECTX12AGILITYSDK_ROOT_DIR}/bin/x64/D3D12Core.dll
-            ${DIRECTX12AGILITYSDK_ROOT_DIR}/bin/x64/d3d12SDKLayers.dll
-            ${CMAKE_BINARY_DIR}/bin/
-)
-
-add_custom_target(
-        DirectX12AgilitySdk_CopyDirectxDlls
-    DEPENDS
-        ${CMAKE_BINARY_DIR}/bin/D3D12Core.dll
-        ${CMAKE_BINARY_DIR}/bin/d3d12SDKLayers.dll
-)
-add_dependencies(DirectX12AgilitySdk DirectX12AgilitySdk_CopyDirectxDlls)
+#add_custom_command(
+#    OUTPUT
+#        ${CMAKE_BINARY_DIR}/bin/D3D12Core.dll
+#        ${CMAKE_BINARY_DIR}/bin/d3d12SDKLayers.dll
+#    COMMAND
+#        ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/bin/
+#    COMMAND
+#        ${CMAKE_COMMAND} -E copy_if_different
+#            ${DIRECTX12AGILITYSDK_ROOT_DIR}/bin/x64/D3D12Core.dll
+#            ${DIRECTX12AGILITYSDK_ROOT_DIR}/bin/x64/d3d12SDKLayers.dll
+#            ${CMAKE_BINARY_DIR}/bin/
+#)
+#
+#add_custom_target(
+#        DirectX12AgilitySdk_CopyDirectxDlls
+#    DEPENDS
+#        ${CMAKE_BINARY_DIR}/bin/D3D12Core.dll
+#        ${CMAKE_BINARY_DIR}/bin/d3d12SDKLayers.dll
+#)
+#add_dependencies(DirectX12AgilitySdk DirectX12AgilitySdk_CopyDirectxDlls)
