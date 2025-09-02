2f7169625ad4392ddebea970a96b07030dd4f98b
Kirill Markin <kmarkindev@gmail.com>
2025-09-02 14:23:44 +0300

    Initialize GBuffer in renderer

--- Source/Runtime/Renderer/Deferred/Include/Runtime/Renderer/Deferred/DeferredRenderer.h
+++ Source/Runtime/Renderer/Deferred/Include/Runtime/Renderer/Deferred/DeferredRenderer.h
@@ -30,9 +30,41 @@ namespace krendrr::Runtime::Renderer::Deferred
         std::shared_ptr<Core::Scene> Scene {};
 
         Microsoft::WRL::ComPtr<ID3D12Fence> RenderFence {};
-        int FenceValue {};
+        int RenderFenceValue {};
 
         bool WaitDirectQueue();
+
+        struct GBuffer
+        {
+            glm::ivec2 Size {-1, -1};
+
+            Microsoft::WRL::ComPtr<ID3D12Resource> DiffuseTexture {};
+            Microsoft::WRL::ComPtr<ID3D12Resource> WorldPositionTexture {};
+            Microsoft::WRL::ComPtr<ID3D12Resource> WorldNormalTexture {};
+            Microsoft::WRL::ComPtr<ID3D12Resource> MetallicTexture {};
+            Microsoft::WRL::ComPtr<ID3D12Resource> RoughnessTexture {};
+            Microsoft::WRL::ComPtr<ID3D12Resource> EmissiveTexture {};
+            Microsoft::WRL::ComPtr<ID3D12Resource> DepthStencilTexture {};
+
+            // Descriptors follow same order as textures are declared in this struct
+            Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> CpuRtvDescriptorHeap {};
+            Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> CpuSrvDescriptorHeap {};
+            inline constexpr static int TEXTURES_COUNT = 6;
+
+            // Contains only one descriptor
+            Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> CpuDsvDescriptorHeap {};
+        };
+
+        GBuffer GBuffer {};
+
+        struct GBufferInitResult
+        {
+            bool bSuccess {};
+            bool bRecordedCommands {};
+        };
+
+        // Called every time we need to update GBuffer, so it has same size as scene view
+        GBufferInitResult InitGBufferForView(const Core::SceneView& SceneView, ID3D12GraphicsCommandList* CommandList);
 
     };
 }
--- Source/Runtime/Renderer/Deferred/Source/DeferredRenderer.cpp
+++ Source/Runtime/Renderer/Deferred/Source/DeferredRenderer.cpp
@@ -48,22 +48,31 @@ bool DeferredRenderer::Render(const std::span<Core::SceneView>& SceneViews)
             "Failed to create command list"
         )
 
+        {
+            const GBufferInitResult Result = InitGBufferForView(SceneView, CommandList.Get());
+
+            if (!Result.bSuccess)
+            {
+                // TODO: log error
+                return false;
+            }
+        }
+
         SceneView.TransitionIntoRenderTargetState(CommandList.Get());
 
         const static D3D12_RECT ScissorRect = CD3DX12_RECT(0, 0, LONG_MAX, LONG_MAX);
         D3D12_VIEWPORT SceneViewViewport = SceneView.GetD3dViewport();
 
         CommandList->RSSetViewports(1, &SceneViewViewport);
-        CommandList->RSSetScissorRects(1, &ScissorRect);
 
-        FLOAT ClearColor[4] = {0.3, 0.5, 0.25, 1.0};
+        constexpr FLOAT ClearColor[4] = {0.3, 0.5, 0.25, 1.0};
         CommandList->ClearRenderTargetView(RenderTargetHandle, ClearColor, 0, nullptr);
 
         CommandList->OMSetRenderTargets(1, &RenderTargetHandle, true, nullptr);
 
         SceneView.TransitionIntoOriginalState(CommandList.Get());
 
-        CommandList->Close();
+        CHECKED_S(CommandList->Close());
         ID3D12CommandList* CommandLists[] = {CommandList.Get()};
 
         RenderApi->GetDirectQueue()
@@ -84,13 +93,137 @@ bool DeferredRenderer::WaitDirectQueue()
 {
     CHECKED(
         RenderApi->GetDirectQueue()
-            ->Signal(RenderFence.Get(), ++FenceValue),
+            ->Signal(RenderFence.Get(), ++RenderFenceValue),
         "Failed to signal Fence"
     )
 
     CHECKED(
-        RenderFence->SetEventOnCompletion(FenceValue, nullptr),
+        RenderFence->SetEventOnCompletion(RenderFenceValue, nullptr),
         "Failed to wait for Fence"
     )
+
+    return true;
 }
+
+DeferredRenderer::GBufferInitResult DeferredRenderer::InitGBufferForView(const Core::SceneView& SceneView, ID3D12GraphicsCommandList* CommandList)
+{
+    const glm::ivec2 ViewportSize = SceneView.GetViewportSize();
+
+    if (GBuffer.Size == ViewportSize)
+        return {true, false};
+
+    auto CreateTextureBuffer = [&](Microsoft::WRL::ComPtr<ID3D12Resource>& TextureBuffer, DXGI_FORMAT Format) -> bool
+    {
+        D3D12_HEAP_PROPERTIES HeapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+        D3D12_RESOURCE_DESC ResourceDesc = CD3DX12_RESOURCE_DESC::Tex2D(Format, ViewportSize.x, ViewportSize.y, 1, 1);
+
+        CHECKED(
+            RenderApi->GetDevice()
+                ->CreateCommittedResource(
+                    &HeapProperties,
+                    D3D12_HEAP_FLAG_NONE,
+                    &ResourceDesc,
+                    D3D12_RESOURCE_STATE_COMMON,
+                    nullptr,
+                    IID_PPV_ARGS(&TextureBuffer)
+                ),
+            "Can't create texture for gbuffer"
+        )
+
+        return true;
+    };
+
+    const bool bError = !CreateTextureBuffer(GBuffer.DiffuseTexture, DXGI_FORMAT_R8G8B8A8_UNORM)
+        || !CreateTextureBuffer(GBuffer.WorldPositionTexture, DXGI_FORMAT_R32G32B32_FLOAT)
+        || !CreateTextureBuffer(GBuffer.WorldNormalTexture, DXGI_FORMAT_R32G32B32_FLOAT)
+        || !CreateTextureBuffer(GBuffer.MetallicTexture, DXGI_FORMAT_R8_UNORM)
+        || !CreateTextureBuffer(GBuffer.RoughnessTexture, DXGI_FORMAT_R8_UNORM)
+        || !CreateTextureBuffer(GBuffer.EmissiveTexture, DXGI_FORMAT_R8G8B8A8_UNORM)
+        || !CreateTextureBuffer(GBuffer.DepthStencilTexture, DXGI_FORMAT_D24_UNORM_S8_UINT);
+
+    if (bError)
+        return {false, false};
+
+    std::array Textures {
+        std::addressof(GBuffer.DiffuseTexture),
+        std::addressof(GBuffer.WorldPositionTexture),
+        std::addressof(GBuffer.WorldNormalTexture),
+        std::addressof(GBuffer.MetallicTexture),
+        std::addressof(GBuffer.RoughnessTexture),
+        std::addressof(GBuffer.EmissiveTexture)
+    };
+
+    // Create RTV descriptors
+    {
+        D3D12_DESCRIPTOR_HEAP_DESC HeapDesc = {
+            .Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+            .NumDescriptors = 6,
+            .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE
+        };
+
+        CHECKED(
+            RenderApi->GetDevice()
+                ->CreateDescriptorHeap(&HeapDesc, IID_PPV_ARGS(&GBuffer.CpuRtvDescriptorHeap)),
+            "Can't create cpu rtv descriptor heap for gbuffer"
+        )
+
+        CD3DX12_CPU_DESCRIPTOR_HANDLE Handle {GBuffer.CpuRtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart()};
+        const unsigned IncrementSize = RenderApi->GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+        for (const auto & Texture: Textures)
+        {
+            RenderApi->GetDevice()
+                ->CreateRenderTargetView(Texture->Get(), nullptr, Handle);
+
+            Handle.Offset(1, IncrementSize);
+        }
+    }
+
+    // Create SRV descriptors
+    {
+        D3D12_DESCRIPTOR_HEAP_DESC HeapDesc = {
+            .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+            .NumDescriptors = 6,
+            .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE
+        };
+
+        CHECKED(
+            RenderApi->GetDevice()
+                ->CreateDescriptorHeap(&HeapDesc, IID_PPV_ARGS(&GBuffer.CpuSrvDescriptorHeap)),
+            "Can't create cpu srv descriptor heap for gbuffer"
+        )
+
+        CD3DX12_CPU_DESCRIPTOR_HANDLE Handle {GBuffer.CpuSrvDescriptorHeap->GetCPUDescriptorHandleForHeapStart()};
+        const unsigned IncrementSize = RenderApi->GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+        for (const auto & Texture: Textures)
+        {
+            RenderApi->GetDevice()
+                ->CreateShaderResourceView(Texture->Get(), nullptr, Handle);
+
+            Handle.Offset(1, IncrementSize);
+        }
+    }
+
+    // Create DSV descriptor
+    {
+        D3D12_DESCRIPTOR_HEAP_DESC HeapDesc = {
+            .Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV,
+            .NumDescriptors = 1,
+            .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE
+        };
+
+        CHECKED(
+            RenderApi->GetDevice()
+                ->CreateDescriptorHeap(&HeapDesc, IID_PPV_ARGS(&GBuffer.CpuDsvDescriptorHeap)),
+            "Can't create cpu dsv descriptor heap for gbuffer"
+        )
+
+        CD3DX12_CPU_DESCRIPTOR_HANDLE Handle {GBuffer.CpuSrvDescriptorHeap->GetCPUDescriptorHandleForHeapStart()};
+        RenderApi->GetDevice()->CreateDepthStencilView(GBuffer.DepthStencilTexture.Get(), nullptr, Handle);
+    }
+
+    GBuffer.Size = ViewportSize;
+
+    return {true, true};
+}
+
 }
